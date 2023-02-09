90339ae00d5dbe4bdd9059c0524e5050583a4381
pedropablomolina <ppmolinaarol@gmail.com>
2023-02-09 11:51:03 +0100

    Session5

--- Input_example.py
+++ Input_example.py
@@ -4,13 +4,14 @@ try:
     age = int(age)
     birth_year = 2023 - age
 except ValueError:age
-    print("sorry, that was not a valid number")
+    print( "sorry, that was not a valid number" )
 except NameError:
     print("sorry, that was not a valid number")
 else:
     print(name, "you were born in", birth_year)
 finally:
     print("Thanks for playing, come again")
+
 
 
 
---
+++ if_example.py
@@ -0,0 +1,7 @@
+number = 5
+
+if number > 5:
+    print(number, "is greater than 5")
+else:
+    print(number, "is  NOT greater than 5")
+
