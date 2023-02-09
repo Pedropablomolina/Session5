70536b572fcc7b6d65b3782a58837d7102924cd7
pedropablomolina <ppmolinaarol@gmail.com>
2023-02-09 11:30:44 +0100

    Session5

---
+++ Input_example.py
@@ -0,0 +1,17 @@
+name = input("What is your name?")
+age = int(input("How old will you be this year?"))
+try:
+    age = int(age)
+    birth_year = 2023 - age
+except ValueError:age
+    print("sorry, that was not a valid number")
+except NameError:
+    print("sorry, that was not a valid number")
+else:
+    print(name, "you were born in", birth_year)
+finally:
+    print("Thanks for playing, come again")
+
+
+
+
